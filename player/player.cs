7d225b3fff9c348dc417a39ca2a16a746cea6618
jrichardroehm <47324417+jrichardroehm@users.noreply.github.com>
2025-02-20 07:03:21 -0500

    Added inflate/deflate mechanics to Blubby movement

--- player/player.cs
+++ player/player.cs
@@ -7,7 +7,7 @@ public partial class Player : CharacterBody2D
 
 	private const float WALK_SPEED = 300.0f;
 	private const float ACCELERATION_SPEED = WALK_SPEED * 6.0f;
-	private const float JUMP_VELOCITY = -725.0f;
+	private const float JUMP_VELOCITY = -125.0f;
 	private const float TERMINAL_VELOCITY = 700.0f;
 	private const float DRIFT_SPEED = 0.0f;
 	private const float DRIFT_DIRECTION = 0.0f;
@@ -20,7 +20,9 @@ public partial class Player : CharacterBody2D
 	[Export]
 	public string ActionSuffix { get; set; } = "";
 
-	private int gravity = (int)ProjectSettings.GetSetting("physics/2d/default_gravity");
+	private int gravity = 0;
+	
+	//(int)ProjectSettings.GetSetting("physics/2d/default_gravity");
 
 	private RayCast2D platformDetector;
 	private AnimationPlayer animationPlayer;
@@ -53,13 +55,17 @@ public partial class Player : CharacterBody2D
 
 		if (Input.IsActionJustPressed("jump" + ActionSuffix))
 		{
-			TryJump();
+			TryInflate();
 		}
 		else if (Input.IsActionJustReleased("jump" + ActionSuffix) && Velocity.Y < 0.0f)
 		{
 			// Reduce vertical momentum if the jump button is released early
-			Velocity = new Vector2(Velocity.X, Velocity.Y * 0.6f);
+			//Velocity = new Vector2(Velocity.X, Velocity.Y * 0.6f);
 		}
+		if(Input.IsActionJustReleased("deflate" + ActionSuffix)){
+			Velocity = new Vector2(Velocity.X, -JUMP_VELOCITY+Velocity.Y);
+		}
+		
 
 		// Apply gravity and terminal velocity
 		Velocity = new Vector2(Velocity.X, Mathf.Min(TERMINAL_VELOCITY, Velocity.Y + gravity * (float)delta));
@@ -116,25 +122,25 @@ public partial class Player : CharacterBody2D
 		return animationNew;
 	}
 
-	private void TryJump()
+	private void TryInflate()
 	{
-		if (IsOnFloor())
-		{
-			jumpSound.PitchScale = 1.0f;
-		}
-		/*		else if (_doubleJumpCharged)
-				{
-					_doubleJumpCharged = false;
-					Velocity = new Vector2(Velocity.X * 2.5f, Velocity.Y);
-					jumpSound.PitchScale = 1.5f;
-				}
-				*/
-		else
-		{
-			return;
-		}
+		// if (IsOnFloor())
+		// {
+		// 	jumpSound.PitchScale = 1.0f;
+		// }
+		// 		else if (_doubleJumpCharged)
+		// 		{
+		// 			_doubleJumpCharged = false;
+		// 			Velocity = new Vector2(Velocity.X * 2.5f, Velocity.Y);
+		// 			jumpSound.PitchScale = 1.5f;
+		// 		}
+		// 		
+		// else
+		// {
+		// 	return;
+		// }
 
-		Velocity = new Vector2(Velocity.X, JUMP_VELOCITY);
+		Velocity = new Vector2(Velocity.X, JUMP_VELOCITY+Velocity.Y);
 		jumpSound.Play();
 	}
 }
